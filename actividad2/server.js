f6988f405b656ebdb39ada3abf929b30b54425d3
ever3520 <everromay@gmail.com>
2026-03-06 19:16:45 -0300

    Agrego último cambio a la actividad 2

--- actividad2/server.js
+++ actividad2/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
-const app = express();
-const PORT = 3000;
+const app = express(); // aqui creo una instancia para express
+const PORT = 3000; // aqui declaro el puerto para el servidor
 
 // Ruta GET con parámetro de consulta
 // Solicitud: http://localhost:3000/profile?edad=30
@@ -11,6 +11,7 @@ app.get('/profile', (req, res) => {
     res.send(`Edad del perfil: ${edad}`);
 });
 
+// Aqui le pido al servidor que escuche por el puerto indicado la petición
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
